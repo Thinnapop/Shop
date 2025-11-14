4a0fac038d01f531c4812c28bee1ae66f2093570
Namo <namo@MacBook-Pro-khxng-Namo.local>
2025-11-15 00:29:27 +0700

    add the api call to make interaction

--- routes/api/products.js
+++ routes/api/products.js
@@ -11,4 +11,60 @@ router.get('/', async(req, res) => {
         res.status(500).json({ message: "Server error occured: ", error: error.message })
     }
 })
+
+// --- GET /api/product/:id ---
+router.get('/:id', async(req, res) => {
+    try {
+        const products = await Product.findById(req.params.id)
+        if (!products) {
+            return res.status(404).json({ message: "Product not found" })
+        }
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({ message: "Server error occured: ", error: error.message })
+    }
+})
+
+// --- POST /api/products/ ---
+router.post('/', async(req, res) => {
+        try {
+            const newProduct = new Product({
+                name: req.body.name,
+                price: req.body.price
+            })
+            const saveProduct = await newProduct.save()
+            res.status(201).json(saveProduct)
+
+        } catch (error) {
+            res.status(500).json({ message: "Invalid data submitted", error: error.message })
+        }
+    })
+    // --- PUT /api/products/:id ---
+router.put('/:id', async(req, res) => {
+    try { //send back new data
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!updatedProduct) {
+            return res.status(404).json({ message: "Product not found" })
+        }
+        res.status(200).json(updatedProduct)
+    } catch (error) {
+        res.status(500).json({ message: "Invalid data for update", error: error.message })
+    }
+})
+
+// --- PUT /api/products/:id ---
+router.delete('/:id', async(req, res) => {
+    try { //send back new data
+        const deleteProduct = await Product.findByIdAndDelete(req.params.id)
+
+        if (!deleteProduct) {
+            return res.status(404).json({ message: "Product not found" })
+        }
+        res.status(200).json({ message: "Product deleted successfully" })
+    } catch (error) {
+        res.status(500).json({ message: "Server bad respond", error: error.message })
+    }
+})
+
 module.exports = router
